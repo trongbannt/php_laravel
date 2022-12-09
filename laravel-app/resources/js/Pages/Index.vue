e17d60107319f3108ea59976f4bd97293a3de91d
bannt <trongbannt@gmail.com>
2022-12-09 18:38:49 +0900

    update code add file FoodItem.vue

--- laravel-app/resources/js/Pages/Index.vue
+++ laravel-app/resources/js/Pages/Index.vue
@@ -1,6 +1,7 @@
 <script setup>
 import MainLayout from '../Layouts/Layout.vue';
 import { Head } from '@inertiajs/inertia-vue3';
+import FoodItem from '../Components/Food/FoodItem.vue';
 
 defineProps({
 
@@ -77,27 +78,26 @@ defineProps({
                             </div>
                         </div>
                     </div>
-                    <!-- Show product -->
+
                     <div class="col-lg-8 col-md-7">
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-sm-6">
-                                <div class="blog__item">
+                                <!-- <div class="blog__item">
                                     <div class="blog__item__pic">
                                         <img src="/blog/blog-2.jpg" alt="">
                                     </div>
                                     <div class="blog__item__text">
                                         <ul>
-                                           
                                             <li><i class="fa fa-calendar-o"></i> May 4,2019</li>
                                             <li><i class="fa fa-comment-o"></i> 5</li>
-                                            <li><i class="fa fa-thin fa-camera"></i> camera</li>
                                         </ul>
                                         <h5><a href="#">6 ways to prepare breakfast for 30</a></h5>
                                         <p>Sed quia non numquam modi tempora indunt ut labore et dolore magnam aliquam
                                             quaerat </p>
                                         <a href="#" class="blog__btn">READ MORE <span class="arrow_right"></span></a>
                                     </div>
-                                </div>
+                                </div> -->
+                                <FoodItem></FoodItem>
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6">
                                 <div class="blog__item">
